105ff88dfe37ddea435d6aa0a4a5d66ceb25d96c
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-02-01 17:33:38 +0600

    Implement navbar and search input components; update layout with new font integration

---
+++ src/app/(home)/search-input.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { SearchIcon, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
+
+export const SearchInput = () => {
+  const [value, setValue] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const hanndleClear = () => {
+    setValue("");
+    inputRef.current?.blur();
+  };
+
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <form action="" className="relative max-w-[720px] w-full">
+        <input
+          value={value}
+          onChange={handleChange}
+          ref={inputRef}
+          placeholder="Search"
+          className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3p_1px_rgba(65,69,73,.15)] bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white"
+        />
+        <Button
+          type="submit"
+          variant={"ghost"}
+          size={"icon"}
+          className="absolute left-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+        >
+          <SearchIcon className="" />
+        </Button>
+        {value && (
+          <Button
+            onClick={hanndleClear}
+            type="button"
+            variant={"ghost"}
+            size={"icon"}
+            className="absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+          >
+            <XIcon className="" />
+          </Button>
+        )}
+      </form>
+    </div>
+  );
+};
