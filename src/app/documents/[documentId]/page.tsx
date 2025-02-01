0da1780d5f94dabd96ce111e2d46c09a85c6e15a
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-02-01 16:23:29 +0600

    Add document input component and navbar integration

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 
 interface DocumentsIdPageProps {
@@ -9,8 +10,14 @@ const DocumentsIdPage = async ({ params }: DocumentsIdPageProps) => {
 
   return (
     <div className="min-h-screen bg-[#fbfbfb] ">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
